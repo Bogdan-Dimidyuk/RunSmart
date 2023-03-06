83402e53a1c615a46d7472b4927113d2a57d670b
Bogdan Dimidyuk <example@gmail.com>
2023-03-06 10:18:46 +0200

    tiny-slider hw

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false,
+    speed: 700
+});
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+});
