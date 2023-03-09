e28a49d14e81892049cfc2be0bc95317e8a7aef1
Bogdan Dimidyuk <example@gmail.com>
2023-03-09 10:03:22 +0200

    adaptive tabs

--- src/js/script.js
+++ src/js/script.js
@@ -13,3 +13,20 @@ document.querySelector('.prev').addEventListener('click', function () {
 document.querySelector('.next').addEventListener('click', function () {
     slider.goTo('next');
 });
+$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+});
+function toggleSlide(item) {
+    $(item).each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            let index = $(this).index(item);
+            $('.catalog-item__content').eq(index).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(index).toggleClass('catalog-item__list_active');
+        })
+    });
+}
+toggleSlide('.catalog-item__link');
+toggleSlide('.catalog-item__back')
